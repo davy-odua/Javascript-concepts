13a052c1198fe702af332b40dffcff1e1d71b457
Davy-odua <davyodua@gmail.com>
2026-04-19 08:14:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//STRINGS//
+// name1 = "Davy"
+// name2 = "Codes"
+
+// names = (name1[0] + name2[0])
+
+// console.log(names[names.length -1])
+
+// strings = "Odua"
+
+// console.log(strings[strings.length -1])
+
+
+// Numbers //
+
+
+//Booleans
+
+
+
+
+
+
+
+
+
+
+
+
