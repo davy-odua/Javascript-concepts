4540b3f25c1f7d99c7373fef4a94ac1b50e78747
Davy-odua <davyodua@gmail.com>
2026-04-24 12:34:16 +0300

    loops

--- script4.js
+++ script4.js
@@ -1,33 +1,35 @@
 //LOOPS
 //Loops repeat the same code over and over again
 //DRY
-// counter = 1
 
-// while (counter <= 20){
-//     console.log(counter)
-//     counter += 1;
-// }
-// console.log("While loop finished running")
+counter = 1
+
+while (counter <= 20){
+    console.log(counter)
+    counter += 1;
+}
+console.log("While loop finished running")
 
 
-// for (let i = 1; i <= 50; i++){
-//     console.log(i) 
+for (let i = 1; i <= 50; i++){
+    console.log(i) 
 
-// }
+}
+
 
 
 //EXAMPLE
-// for (let i = 1; i <= 15; i++ ){
-//     if (i % 5 === 0){
-//         console.log(`${i} ASAP Frontend`)
-//     }
-//     else if (i % 2 === 0 ){
-//         console.log(`${i}  Frontend`)
-//     }
-//     else{
-//         console.log(`${i} ASAP`)
-//     }
-// }
+for (let i = 1; i <= 15; i++ ){
+    if (i % 5 === 0){
+        console.log(`${i} ASAP Frontend`)
+    }
+    else if (i % 2 === 0 ){
+        console.log(`${i}  Frontend`)
+    }
+    else{
+        console.log(`${i} ASAP`)
+    }
+}
 
 //Example2
 str = "ASAP Frontend"
