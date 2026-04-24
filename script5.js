f3fbfbf25798b45970f6937b3b706a4ae197edc3
Davy-odua <davyodua@gmail.com>
2026-04-24 13:45:41 +0300

    functions

--- script5.js
+++ script5.js
@@ -1,7 +1,30 @@
 //FUNCTIONS
+//Functions are resuable blocks of code that perform specific tasks
+
+// function add(numb1, numb2){
+//     return numb1 + numb2
+// }
+
+// console.log(add(5, 4))
+
+//Regular functions
+// function convertUsdToAud (usd){
+//     return usd * 1.5;
+    
+// }
+
+// console.log(convertUsdToAud(1000))
+// console.log(convertUsdToAud(500))
+// console.log(convertUsdToAud(200))
+// console.log(convertUsdToAud(100))
 
 
+// Arrow functions
+const convertUsdToAud2 = (usd) => {
+     return usd * 1.5;
 
+}
+console.log(convertUsdToAud2(1000))
 
 
 
