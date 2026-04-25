8c1f8335d4b7d8ff739be8c466283582633efefd
Davy-odua <davyodua@gmail.com>
2026-04-25 13:03:01 +0300

    DOM

---
+++ dom.js
@@ -0,0 +1,36 @@
+//THE DOM - Allows us to interact with and change the content and styles of 
+//          the elements on our webpage
+
+//Methods of accessing webpage content from our Javascript
+//1st Method
+// document.querySelector('h1')
+
+//How to change html content usign Javascript
+// document.querySelector('h1').innerHTML += " ASAP Backend"
+
+//How to change css content usign Javascript
+// document.querySelector('h1').style.color = 'red'
+
+// function buttonClicked (){
+//     document.querySelector('button').style.backgroundColor = 'red'
+// }
+
+
+//HOW TO CHANGE CLASS NAMES using Js
+function toggleSidebar(){
+    document.querySelector('body').classList.toggle('open')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
